da6a86ad3db4f8b3c26c91b2f15287e73b23d5da
knareshkk <naresh.kalakanti@gmail.com>
2014-10-23 13:02:54 +0100

    created simple server

---
+++ server.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+/*
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello');
+
+}).listen(1337, '127.0.0.1');
+console.log('webservice has started'); */
+
+// To get file system module 
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end(data);
+    });
+}).listen(1337, '127.0.0.1');
+console.log('webservice has started');
+
+
